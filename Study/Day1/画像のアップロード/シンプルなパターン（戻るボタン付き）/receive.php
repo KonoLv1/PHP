8248d2a72daad1e5590b2dec394b42ddd3a2611b
unknown <1809106@s.asojuku.ac.jp>
2019-12-12 12:29:30 +0900

    アップロードのアレコレ

--- Study/Day1/画像のアップロード/シンプルなパターン（戻るボタン付き）/receive.php
+++ Study/Day1/画像のアップロード/シンプルなパターン（戻るボタン付き）/receive.php
@@ -6,6 +6,7 @@
       print "[Success]".($_FILES['upfile']['name'])."をアップロードしました。";
     }
   ?>
+  <p><a href="index.html">アップロード画面に戻る</a></p>
 </body>
 
 <!-- © Junichi Koyama @Twitter@KonoLv1 / https://github.com/KonoLv1 -->
