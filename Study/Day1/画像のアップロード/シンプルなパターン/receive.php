6246a5bc07361247cd8abd9a1e3ca6a1d8eed1f3
KonoLv1 <konolevel1@gmail.com>
2019-12-15 11:49:02 +0900

    画像のアップロードのアレコレ

--- Study/Day1/画像のアップロード/シンプルなパターン/receive.php
+++ Study/Day1/画像のアップロード/シンプルなパターン/receive.php
@@ -1,6 +1,8 @@
 <body>
   <?php
-    if(move_uploaded_file($_FILES['upfile']['tmp_name'],"./".$_FILES['upfile']['name']) == FALSE){
+    // 第２パスには保存先のパスを指定している。正しくてエラーが帰ってくる場合は大体フォルダの権限（パーミッション）設定が７７７になっていないゾ
+    // フォルダは事前に作っておくように、フォルダ名を「images」以外にする場合は下記のimagesをフォルダ名と同じものに変更することゾ
+    if(move_uploaded_file($_FILES['upfile']['tmp_name'],'./images/'.$_FILES['upfile']['name']) == FALSE){
       print "[ERROR]アップロードに失敗しました";
     }else{
       print "[Success]".($_FILES['upfile']['name'])."をアップロードしました。";
